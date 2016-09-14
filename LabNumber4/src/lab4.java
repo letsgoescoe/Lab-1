5734471f07e6c0b2c4ba4c7528ca4f866e1be2a3
Dominique Escoe <descoe2001@yahoo.com git config --global user.email descoe2001@yahoo.com>
2016-09-14 10:10:45 -0400

    modifications

--- LabNumber4/src/lab4.java
+++ LabNumber4/src/lab4.java
@@ -8,29 +8,34 @@ public class lab4 {
 		
 		//Declarations
 				int input;
-				long fact= 1;
+				long fact;
 		
 		//Scan what users enter
 		Scanner userInput = new Scanner(System.in);
 		
 		//While loop bc we will ask the user if they want to continue
 		String choice = "y";
-		while (choice.equals("y")){
-			
+		while (choice.equals("y"))
+		     
+		{
+			//beginning of the game
 			System.out.println("Enter an integer that's greater than 0 but less than 10:");
 			input = userInput.nextInt();
-			
-				for (int i = 1; i <= input; i++)
-		    fact =fact*=i;       
+			fact = 1;
+			//For loop to complete math	
+			for (int i = 1; i <= input; i++)
+		    fact =fact *i;       
 	   
-
+         //To see if user wants to continue
 	    System.out.println("Factorial: "+fact);
 				System.out.println("The factorial for input =");
 				System.out.print("Continue? (y/n): ");
 						choice = userInput.next();
+						
+						
 	
 		}
-		
+		System.out.println("Thanks for playing!");
 	}
 	
 }
